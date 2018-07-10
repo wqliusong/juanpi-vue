498ed56869949e33e604ce150dac82e578d4757e
ls_003 <759538462@qq.com>
2018-07-10 14:23:26 +0800

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="navbarBox">
-    <mt-navbar v-model="selected" @click="item">
-      <mt-tab-item id="1">选项一</mt-tab-item>
-      <mt-tab-item id="2">选项二</mt-tab-item>
-      <mt-tab-item id="3">选项三</mt-tab-item>
-    </mt-navbar>
+    <div v-on:click="item(selected)">
+      <mt-navbar v-model="selected">
+        <mt-tab-item :id="1">选项一</mt-tab-item>
+        <mt-tab-item :id="2">选项二</mt-tab-item>
+        <mt-tab-item :id="3">选项三</mt-tab-item>
+      </mt-navbar>
+    </div>
+    <mt-cell class="page-part" title="当前选中">{{ selected }}</mt-cell>
+    <!-- tab-container -->
     <mt-tab-container v-model="selected">
-      <mt-tab-container-item id="1">
+      <mt-tab-container-item :id="1">
         <mt-cell v-for="n in 10" :key="n" :title="'内容 ' + n" />
       </mt-tab-container-item>
-      <mt-tab-container-item id="2">
+      <mt-tab-container-item :id="2">
         <mt-cell v-for="n in 4" :key="n" :title="'测试 ' + n" />
       </mt-tab-container-item>
-      <mt-tab-container-item id="3">
+      <mt-tab-container-item :id="3">
         <mt-cell v-for="n in 6" :key="n" :title="'选项 ' + n" />
       </mt-tab-container-item>
     </mt-tab-container>
@@ -20,15 +24,14 @@
 </template>
 <script>
 export default {
-  name: 'navbar',
   data () {
     return {
-      selected: 2
+      selected: 1
     }
   },
   methods: {
-    item: (data) => {
-      console.log(data)
+    item: (selected) => {
+      console.log(selected)
     }
   }
 }
